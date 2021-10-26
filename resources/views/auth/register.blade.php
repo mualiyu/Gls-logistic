2b010dc0a83a0f368ea48b5a2ca7ee0139befa86
nualiyu <mualiyuoox@gmaill.com>
2021-10-26 15:43:00 +0100

    amount is correction

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,26 @@
-@extends('layouts.app')
+@extends('layouts.Admin.index')
 
 @section('content')
-<div class="container">
+    <div class="page-breadcrumb bg-white">
+                <div class="row align-items-center">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">Register User</h4>
+                    </div>
+                    <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                        <div class="d-md-flex">
+                            <ol class="breadcrumb ms-auto">
+                                <li><a href="#" class="fw-normal">User</a></li>
+                            </ol>
+                           
+                        </div>
+                    </div>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <div class="container-fluid">
+
     <div class="row justify-content-center">
+        
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
