fb6efbe45e75ed8f98e09146b73a7a3d37f79b6f
nualiyu <mualiyuoox@gmaill.com>
2021-10-28 23:20:11 +0100

    update with Upload image

--- resources/views/admin/package/info.blade.php
+++ resources/views/admin/package/info.blade.php
@@ -98,10 +98,12 @@
 					    </div>
 				    </div>
                                     <div class="form-group mb-4">
-                                        <label for="example-email" class="col-md-12 p-0">Item type:</label>
+                                        <label for="example-email" class="col-md-12 p-0">Items:</label>
                                         <div class="col-md-12 border-bottom px-4">
                                             <span class="form-control px-5 border-0">
-						    {{$package->item_type}}
+						    @foreach ($package->items as $item)
+						   [ Type: {{$item->name}}, Weight: {{$item->weight}} ], 
+						    @endforeach
 						</span>
                                         </div>
                                     </div>
